03525ddc31af4f63ca2484b9144d97f49c0bcca1
Wolfgang Kühn <wolfgang-kuehn@decatur.de>
2020-09-22 13:51:46 +0200

    refactored app server examples

---
+++ whatchamacallit/fastapi.py
@@ -0,0 +1,35 @@
+import mimetypes
+import pathlib
+from typing import Dict
+
+from fastapi import FastAPI
+from starlette.exceptions import HTTPException
+from starlette.responses import Response
+
+from whatchamacallit import make
+from whatchamacallit.make import ImportToPackageMapper
+
+
+def import_remap(path: pathlib.Path, spec_mapping: Dict[str, str]):
+    source = make.resolve_package_resource(path)
+
+    if not source.is_file():
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return Response(content=make.read_file(source, spec_mapping), media_type=mimetypes.guess_type(source.name)[0])
+
+
+def register(app: FastAPI):
+    spec_mapping = ImportToPackageMapper()
+
+    @app.get(r"/{file_path:path}\.js")
+    def read_js(file_path: str):
+        return import_remap(pathlib.Path(file_path + '.js'), spec_mapping)
+
+    @app.get(r"/{file_path:path}\.html")
+    def read_html(file_path: str):
+        return import_remap(pathlib.Path(file_path + '.html'), spec_mapping)
+
+    @app.get(r"/{file_path:path}\.css")
+    def read_css(file_path: str):
+        return import_remap(pathlib.Path(file_path + '.css'), spec_mapping)
---
+++ whatchamacallit/flask.py
@@ -0,0 +1,25 @@
+import mimetypes
+import pathlib
+from typing import Dict
+
+from flask import Flask, request, Response
+from werkzeug.exceptions import NotFound
+
+from whatchamacallit import make
+from whatchamacallit.make import ImportToPackageMapper
+
+
+def import_remap(path: pathlib.Path, spec_mapping: Dict[str, str]):
+    if path.suffix not in {'.js', '.html', '.css'}:
+        return
+
+    source = make.resolve_package_resource(path)
+    if not source.is_file():
+        raise NotFound()
+
+    return Response(make.read_file(source, spec_mapping), mimetype=mimetypes.guess_type(source.name)[0])
+
+
+def register(app: Flask):
+    spec_mapping = ImportToPackageMapper()
+    app.before_request(lambda: import_remap(pathlib.Path(request.path[1:]), spec_mapping))
